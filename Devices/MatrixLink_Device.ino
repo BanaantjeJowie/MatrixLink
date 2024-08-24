54a24f323ebe54a6466efb70d55b1430672efec3
joe boone <joejoboone@hotmail.com>
2024-08-24 15:13:08 +0200

    additions

---
+++ Devices/MatrixLink_Device.ino
@@ -0,0 +1,12 @@
+// This is the code that is running on every MatrixLink device.
+// It is connected to the LED matrix and the tft screen and is responsible for displaying the configutation saved in the database on the server.
+// The device refreshes after a set period of time to get the latest configuration from the server.
+
+void setup() {
+  // Initialisation
+  
+}
+
+void loop() {
+    // loop
+}
